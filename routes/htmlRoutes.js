f71a70cda0205c7fae291cb0014ca38db51156c8
Alex Camacho <alex.c@WalkMe.local>
2018-09-12 20:52:20 -0700

    fixed routes, got css and js files to load, remove jquery slim versions

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,41 +2,70 @@ var db = require("../models");
 var path = require("path");
 
 
-
 module.exports = function (app) {
 
-  // Should be for main page
-  app.get("/", function (req, res) {
-    res.sendFile(path.resolve("./views/index.html"))
-  });
 
-  app.get("/index", function (req, res) {
-    res.sendFile(path.resolve("./views/index.html"))
-  });
+
   // This gets the translations page to display it using Express.
   app.get("/translation", function (req, res) {
     res.sendFile(path.resolve("./views/translation.html"))
   })
+  app.get("/signup", function (req, res) {
+    res.sendFile(path.resolve("./views/signup.html"))
+  })
+  app.get("/login", function (req, res) {
+    res.sendFile(path.resolve("./views/login.html"))
+  })
+
   // For the location page
   app.get("/locations", function (req, res) {
     res.sendFile(path.resolve("./views/location.html"))
   })
   //Profile page
   app.get("/profile", function (req, res) {
-    res.sendFile(path.resolve("./views/profile.html"))
-  })
 
-  app.get("/public/styles/main.css", function (req, res) {
-    res.sendFile(path.resolve("./public/styles/main.css"))
-  })
 
-  app.get("/public/js/geolocation.js", function (req, res) {
-    res.sendFile(path.resolve("./public/js/geolocation.js"))
+    console.log('Cookies: ', req.cookies);
+    try {
+      if (req.cookies["userinfo"] == undefined) {
+        res.redirect("/signup")
+      } else {
+        res.sendFile(path.resolve("./views/profile.html"))
+      }
+    } catch (error) {
+      res.redirect("/signup")
+    }
   })
+  app.post("/profile", function (req, res) {
 
-  app.get("/public/js/handleTranslation.js", function (req, res) {
-    res.sendFile(path.resolve("./public/js/handleTranslation.js"))
+
+    console.log('Cookies: ', req.cookies);
+    try {
+      if (req.cookies["userinfo"] == undefined) {
+        res.redirect("/signup")
+      } else {
+        res.sendFile(path.resolve("./views/profile.html"))
+      }
+    } catch (error) {
+      res.redirect("/signup")
+    }
   })
+  /* 
+   app.get("/public/styles/main.css", function (req, res) {
+     res.sendFile(path.resolve("./public/styles/main.css"))
+   })
+
+   app.get("/public/js/geolocation.js", function (req, res) {
+     res.sendFile(path.resolve("./public/js/geolocation.js"))
+   })
+
+   app.get("/public/js/handleTranslation.js", function (req, res) {
+     res.sendFile(path.resolve("./public/js/handleTranslation.js"))
+   }) */
+  // Should be for main page
+  app.get("/*", function (req, res) {
+    res.sendFile(path.resolve("./views/index.html"))
+  });
 
   // Load example page and pass in an example by id
   /*   app.get("/example/:id", function (req, res) {
@@ -52,8 +81,4 @@ module.exports = function (app) {
     });
     */
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
-    res.render("404");
-  });
 };
